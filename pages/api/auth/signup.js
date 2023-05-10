ad5753aba86ab081e2a18dc270bafa1d4cbf29a9
Sheila Sharon <70254321+DevSheila@users.noreply.github.com>
2023-05-10 17:16:16 +0300

    Update signup.js

--- pages/api/auth/signup.js
+++ pages/api/auth/signup.js
@@ -1,4 +1,5 @@
 import { MongoClient } from 'mongodb';
+import { hash } from 'bcryptjs';
 
 export default async function handler(req, res) {
   if (req.method === 'POST') {
@@ -20,7 +21,7 @@ export default async function handler(req, res) {
       }
 
       // Create a new user
-      const result = await db.collection('users').insertOne({ username, email, password });
+      const result = await db.collection('users').insertOne({ username, email,  password : await hash(password, 12) });
       res.status(201).json({ message: 'User created' });
     } catch (error) {
       res.status(500).json({ message: 'Something went wrong' });
@@ -31,3 +32,5 @@ export default async function handler(req, res) {
     res.status(400).json({ message: 'Invalid request method' });
   }
 }
+
+
